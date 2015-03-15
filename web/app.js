0e6b749a6e019dc3a0568746599cca574352eace
norseboar <reed@outlook.com>
2015-03-14 21:52:44 -0700

    add handlebars template engine to app.js

--- web/app.js
+++ web/app.js
@@ -1,15 +1,19 @@
 var express = require('express');
+var hbs = require('hbs');
+
 var app = express();
 
 app.set('port', process.env.PORT || 3000);
+app.set('view engine', 'html');
+app.engine('html', hbs.__express);
 
 app.get('/', function(req, res){
-  res.sendFile('views/index.html', {"root": __dirname});
+  res.render('index');
 });
 
 var server = app.listen(app.get('port'), function() {
   var host = server.address().address;
   var port = server.address().port;
 
-  console.log('Example app listening at http://%s:%s', host, port);
+  console.log('Example app listening at http://localhost:%s', port);
 });
