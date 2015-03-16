0c21c998d8e84bd9b36020946942ad5b29a5e5ad
norseboar <reed@outlook.com>
2015-03-15 20:14:58 -0700

    moving from hbs to express-hbs for documentation

--- web/app.js
+++ web/app.js
@@ -1,11 +1,14 @@
+var path = require('path');
 var express = require('express');
-var hbs = require('hbs');
+var hbs = require('express-hbs');
 var data = require('./data');
 
 var app = express();
 app.set('port', process.env.PORT || 3000);
 app.set('view engine', 'hbs');
-app.engine('hbs', hbs.__express);
+app.engine('hbs', hbs.express4({
+  defaultLayout: path.join(__dirname, 'views/layout.hbs')
+}));
 
 app.get('/', function(req, res){
   res.render('index', {claims: data.getClaims});
