699b4a9b88f6c3915e60f26b1fef52bc7db712db
norseboar <reed@outlook.com>
2015-03-15 20:16:55 -0700

    more changes to make solid base
    -mongodb and monk added as dependencies
    -error handling added to app.js
    -views folder added
    -module.exports added for app.js

--- web/app.js
+++ web/app.js
@@ -5,10 +5,44 @@ var data = require('./data');
 
 var app = express();
 app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 app.engine('hbs', hbs.express4({
   defaultLayout: path.join(__dirname, 'views/layout.hbs')
 }));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Catch 404 and forwarding to error handler
+// TODO: add other 'use' statements so that good paths are caught first. This
+// one should act as a catch-all  for other paths
+
+// app.use(function(req, res, next){
+//   var err = new Error('Not Found');
+//   err.status = 404;
+//   next(err)
+// });
+
+// Error handlers
+if (app.get('env') === 'development'){
+  app.use(function(err, req, res, next){
+    res.status(err.status || 500);
+    res.render('error', {
+      message:err.message,
+      error: err
+    });
+  });
+}
+
+app.use(function(err, req, res, next){
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+module.exports = app;
 
 app.get('/', function(req, res){
   res.render('index', {claims: data.getClaims});
