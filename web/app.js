1d011539e0604215da86342b6b5c132bbaaf14ca
norseboar <reed@outlook.com>
2015-03-15 17:54:03 -0700

    page displays data from data.js

--- web/app.js
+++ web/app.js
@@ -1,14 +1,14 @@
 var express = require('express');
 var hbs = require('hbs');
+var data = require('./data');
 
 var app = express();
-
 app.set('port', process.env.PORT || 3000);
 app.set('view engine', 'hbs');
 app.engine('hbs', hbs.__express);
 
 app.get('/', function(req, res){
-  res.render('index');
+  res.render('index', {claims: data.getClaims});
 });
 
 var server = app.listen(app.get('port'), function() {
