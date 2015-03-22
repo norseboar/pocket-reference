0ccb4953990bbf2b4643cdc6f36397f8a1362ede
norseboar <reed@outlook.com>
2015-03-22 10:53:29 -0700

    Create views for registration and login
    Neither are functional yet, and message-passing has been commented out
    for now

--- web/routes/index.js
+++ web/routes/index.js
@@ -1,13 +1,57 @@
 var express = require('express');
 var router = express.Router();
 
-// GET home page
-router.get('/', function(req, res, next){
+// Authentication logic
+var isLoggedIn = function(req, res, next){
+  if(req.isAuthenticated()){
+    return next();
+  }
+  else {
+    res.redirect('/');
+  }
+};
+
+// HOME PAGE ==================================================================
+router.get('/', function(req, res){
+  if(false){
+  }
+  else {
+    // If user is not logged in, show landing page
+    res.render('index');
+  }
+});
+
+// CLAIMS PAGE (default for logged in users)
+router.get('/=', isLoggedIn, function(req, res){
+  // If user is logged in, show their claims
   var db = req.db;
   var collection = db.get('claimscollection');
   collection.find({},function(e, docs){
-    res.render('index', {claims: docs})
+    res.render('claims', {claims: docs, user: req.user})
   });
+})
+
+// LOGIN ======================================================================
+// show login form
+router.get('/login', function(req, res){
+  // render, passing in any flash data (if it exists)
+  // res.render('login', { message: req.flash('loginMessage')});
+  res.render('login');
+});
+
+// process login form
+
+// REGISTER ===================================================================
+router.get('/register', function(req, res){
+  // render, passing in any flash data (if it exists)
+  // res.render('register', {message: req.flash('registerMessage')});
+  res.render('register');
+});
+
+// LOGOUT =====================================================================
+router.get('/logout', function(req, res){
+  req.logout();
+  res.redirect('/');
 });
 
 module.exports = router;
