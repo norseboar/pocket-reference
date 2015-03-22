2b8ad14639de41424f3f72c00c88290f6433188b
norseboar <reed@outlook.com>
2015-03-21 21:26:47 -0700

    clean up app.js setup, add new modules
    morgan (a logger), body-parser, and cookie-parser all added as
    dependencies

--- web/app.js
+++ web/app.js
@@ -3,28 +3,37 @@ var express = require('express');
 var hbs = require('express-hbs');
 var mongo = require('mongodb');
 var monk = require('monk');
+var morgan = require('morgan');
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
 
-var routes = require(path.join(__dirname, 'routes/index'));
-
-console.log(process.env.MONGOLAB_URI);
-var db = monk(process.env.MONGOLAB_URI);
-
+// configuration ==============================================================
 var app = express();
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Set up handlebars
 app.set('view engine', 'hbs');
 app.engine('hbs', hbs.express4({
   defaultLayout: path.join(__dirname, 'views/layout.hbs')
 }));
 
-app.use(express.static(path.join(__dirname, 'public')));
-
-// Enable MongoDB
+// Connect to MongoDB
 // Every request is probably overkill, this is a rough solution
+var db = monk(process.env.MONGOLAB_URI);
 app.use(function(req, res, next){
   req.db = db;
   next();
 });
+
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({ extended:false }));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+// routing ====================================================================
+var routes = require(path.join(__dirname, 'routes/index'));
 app.use('/', routes);
 
 // Catch 404 and forwarding to error handler
@@ -55,6 +64,7 @@ app.use(function(err, req, res, next){
 
 module.exports = app;
 
+// launch =====================================================================
 var server = app.listen(app.get('port'), function() {
   var host = server.address().address;
   var port = server.address().port;
