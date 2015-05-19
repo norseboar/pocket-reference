0f7b52c751cbd7007db216bab8660d91f740b093
norseboar <reed@outlook.com>
2015-05-18 22:57:25 -0700

    change server start text to be more accurate
    Before, always stated that server was started on localhost

--- web/app.js
+++ web/app.js
@@ -78,8 +78,7 @@ var server = app.listen(app.get('port'), function() {
   var host = server.address().address;
   var port = server.address().port;
 
-  console.log('Pocket Reference server listening at' +
-      'http://localhost:%s', port);
+  console.log('Pocket Reference server listening on port ', port);
 });
 
 io.listen(server);
